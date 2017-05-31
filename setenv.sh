b8dc50b5d62b47f4c4693fe37f2bd7e6600ba4b0
Ravi Srinivasan <rsriniva@redhat.com>
2017-05-31 18:24:54 +0530

    Added short description and a setenv script

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MONGODB_USER=taxcalc
+export MONGODB_DATABASE=taxcalcdb
+export MONGODB_PASSWORD=taxcalc
+export TAXCALCDB_SERVICE_HOST=localhost
+export TAXCALCDB_SERVICE_PORT=27017
