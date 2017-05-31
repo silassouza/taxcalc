d31b301ecc5cb842efb20e0aa04361c9f5f8734c
Ravi Srinivasan <rsriniva@redhat.com>
2017-05-31 11:33:31 +0530

    Added more saner log output and added a new route to dump hostname info

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     app     = express(),
-    bodyParser = require('body-parser');
-    mongoose   = require('mongoose');
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose'),
+    os = require('os'),
+    hostname = os.hostname();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -9,11 +11,11 @@ app.use(bodyParser.json());
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-var mongoUser =  process.env.MONGODB_USER;
-    mongoDatabase = process.env.MONGODB_DATABASE;
-    mongoPassword = process.env.MONGODB_PASSWORD;
-    mongoHost = process.env.TAXCALCDB_SERVICE_HOST;
-    mongoPort = process.env.TAXCALCDB_SERVICE_PORT;
+var mongoUser =  process.env.MONGODB_USER,
+    mongoDatabase = process.env.MONGODB_DATABASE,
+    mongoPassword = process.env.MONGODB_PASSWORD,
+    mongoHost = process.env.TAXCALCDB_SERVICE_HOST,
+    mongoPort = process.env.TAXCALCDB_SERVICE_PORT,
     mongoURL = 'mongodb://';
 
 mongoURL += mongoUser + ':' + mongoPassword + '@';
@@ -25,6 +27,17 @@ var Transaction = require('./models/transaction');
 
 var route = express.Router();
 
+// All our services are under the /api context
+app.use('/api', route);
+
+// Start defining routes for our app/microservice
+
+// A route that dumps hostname information from pod
+route.get('/', function(req, res) {
+    res.send('Hi! I am running on host -> ' + hostname);
+});
+
+// This route handles tax calculation for our service
 route.route('/calculate')
      .post(function(req, res) {
 
@@ -42,18 +55,7 @@ route.route('/calculate')
 
     });
 
-// All our services are under the /api context
-app.use('/api', route);
-
-// Start defining routes for our app/microservice
-
-route.get('/', function(req, res) {
-    res.json({
-      status: 'up'
-    });
-});
-
 app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+console.log('nodejs server running on http://%s:%s', ip, port);
 
 module.exports = app;
